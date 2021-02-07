dd04f2ae95286107512375753a78838db12f20fc
Bill Harford <bharford@exemple.com>
2021-02-06 23:20:08 -0300

    change noel.py file to resolve any possible test

--- test_noel.py
+++ test_noel.py
@@ -15,4 +15,8 @@ class TestNoel(unittest.TestCase):
     def test_five_Ho(self):
     	self.assertEqual(solution(5), "Ho Ho Ho Ho Ho!")
 
+    def test_ten_Ho(self):
+    	self.assertEqual(solution(10), "Ho Ho Ho Ho Ho Ho Ho Ho Ho Ho!")
+
+
 unittest.main()
